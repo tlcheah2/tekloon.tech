70ba4f1ba197d58196352778be2697c05f374982
Tek Loon <tekloon.1991@gmail.com>
2019-10-28 22:33:40 +0800

    Added project - minimalism-blog

--- src/pages/works.js
+++ src/pages/works.js
@@ -14,6 +14,10 @@ const WorksPage = () => {
                 <h3><Link to="/works/four-card-feature">Four Card Feature Section</Link></h3>
                 <p>This is the solution for <a href="https://www.frontendmentor.io/challenges/four-card-feature-section-weK1eFYK">Four Card Feature Section Challenges</a> in Frontend Mentor.</p>
             </div>
+            <div>
+                <h3><Link to="/works/minimalism-blog/">Minimalism Blog using Pure HTML & CSS</Link></h3>
+                <p>A project of Web Responsive Design Bootcamp by <a href="https://scrimba.com/g/gresponsive">Kevin Powell's course</a></p>
+            </div>
         </Page>
     </Layout>);
 };
