ff54fcee98ee91456b21adbf3712efaafcdc51de
Tek Loon <tekloon.1991@gmail.com>
2020-05-03 17:13:15 +0800

    Added gatsby tagStyles

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -4,6 +4,7 @@ const tagStyle = {
     'javascript': { backgroundColor: '#F0DB4F', color: 'black' },
     'nodejs': { backgroundColor: '#68A063', color: 'white' },
     'angular': { backgroundColor: '#B52E31', color: 'white' },
+    'gatsby': { backgroundColor: '#663399', color: 'white'}
 }
 
 export default (props) => {
