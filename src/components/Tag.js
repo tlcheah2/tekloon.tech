7ba9f3894f5f39278e3330ce323061864ffff7ed
Tek Loon <tekloon.1991@gmail.com>
2020-05-04 21:27:52 +0800

    Added CSS tag style

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -4,7 +4,8 @@ const tagStyle = {
     'javascript': { backgroundColor: '#F0DB4F', color: 'black' },
     'nodejs': { backgroundColor: '#68A063', color: 'white' },
     'angular': { backgroundColor: '#B52E31', color: 'white' },
-    'gatsby': { backgroundColor: '#663399', color: 'white'}
+    'gatsby': { backgroundColor: '#663399', color: 'white'},
+    'css': { backgroundColor: '#2965f1', color: 'white', textTransform: 'uppercase' },
 }
 
 export default (props) => {
