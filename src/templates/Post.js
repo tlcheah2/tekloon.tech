5342dde9cd51cd6ef1372098915fca7cb935a302
Tek Loon <tekloon.1991@gmail.com>
2020-05-09 19:39:09 +0800

    Added ArticleSeo into Post

--- src/templates/Post.js
+++ src/templates/Post.js
@@ -3,6 +3,7 @@ import { graphql } from "gatsby"
 import Layout from '../components/Layout';
 import NavBar from '../components/NavBar';
 import SEO from '../components/Seo';
+import ArticleSeo from '../components/ArticleSeo';
 import styles from './Post.module.css';
 import { defineCustomElements as deckDeckGoHighlightElement } from '@deckdeckgo/highlight-code/dist/loader';
 deckDeckGoHighlightElement();
@@ -10,10 +11,22 @@ deckDeckGoHighlightElement();
 export default ({ data }) => {
     console.log('styles', styles);
     const post = data.markdownRemark;
+    const { siteMetadata } = data.site;
+    const { slug } = post.fields;
     return (
         <>
             <NavBar />
-            <SEO title={post.frontmatter.title} description={post.excerpt} />
+            <SEO title={post.frontmatter.title} description={post.excerpt} isBlogPost={true} />
+            <ArticleSeo 
+              title={post.frontmatter.title} 
+              description={post.excerpt}
+              url={`${siteMetadata.url}${slug}`}
+              isBlogPost={true}
+              datePublished={post.frontmatter.date}
+              author={siteMetadata.author}
+              imageUrl={siteMetadata.image}
+              organization={siteMetadata.organization}
+            />
             <Layout>
                 <h1 className={styles.postTitle}>{post.frontmatter.title}</h1>
                 <p className={styles.publishedDate}>Published {post.frontmatter.date}</p>
@@ -33,6 +46,25 @@ export const query = graphql`
         date
       }
       excerpt
+      fields {
+        slug
+      }
     }
+    site {
+      siteMetadata {
+        title
+        description
+        author
+        url
+        siteUrl
+        twitterUsername
+        image
+        organization {
+          url
+          logo
+          name
+        }
+      }
+    } 
   }
 `
