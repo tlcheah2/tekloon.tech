6061ff2b3f9d58a953ad13f468b785727ca6911a
Tek Loon <tekloon.1991@gmail.com>
2021-02-21 23:43:59 +0800

    feat: add daily aws quiz, template & questions - 21-02-2021

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,22 +1,33 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
+const quizData = require('./quiz.json');
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-    if (node.internal.type === `MarkdownRemark`) {
-        const { createNodeField } = actions;
-        const slug = createFilePath({ node, getNode, basePath: `pages` });
-        const pathBasename = path.basename(slug);      
-        createNodeField({
-            node,
-            name: `slug`,
-            value: pathBasename,
-        })
+  if (node.internal.type === `MarkdownRemark`) {
+    const { createNodeField } = actions;
+    const slug = createFilePath({ node, getNode, basePath: `pages` });
+    if (slug.startsWith('/blog/')) {
+      const pathBasename = path.basename(slug);
+      createNodeField({
+        node,
+        name: `slug`,
+        value: pathBasename,
+      })
+    } else if (slug.startsWith('/daily-quiz/')) {
+      // const pathBasename = path.basename(slug);
+      createNodeField({
+        node,
+        name: `slug`,
+        value: slug,
+      })
     }
+  }
+
 }
 
 exports.createPages = async ({ graphql, actions }) => {
-    const { createPage } = actions;
-    const result = await graphql(`
+  const { createPage } = actions;
+  const result = await graphql(`
       query {
         allMarkdownRemark {
           edges {
@@ -29,15 +40,27 @@ exports.createPages = async ({ graphql, actions }) => {
         }
       }
     `);
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-        createPage({
-          path: node.fields.slug,
-          component: path.resolve(`./src/templates/Post.js`),
-          context: {
-            // Data passed to context is available
-            // in page queries as GraphQL variables.
-            slug: node.fields.slug,
-          },
-        })
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    const comp = node.fields.slug.startsWith('/daily-quiz') ? path.resolve(`./src/templates/Quiz.js`) : path.resolve(`./src/templates/Post.js`)
+    createPage({
+      path: node.fields.slug,
+      component: comp,
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        slug: node.fields.slug,
+      },
     })
-  }
+  })
+  // Create Page for all quiz Data
+  quizData.forEach((quiz, index) => {
+    createPage({
+      path: `/daily-quiz/quiz-${index + 1}`,
+      component: path.resolve(`./src/templates/Quiz.js`),
+      context: {
+        quizNumber: index + 1,
+        ...quiz,
+      }
+    })
+  });
+}
---
+++ src/templates/Quiz.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import Layout from '../components/Layout';
+import NavBar from '../components/NavBar';
+import styles from './Quiz.module.css';
+
+const Quiz = ({ data }) => {
+  const { question, answers, correctAnswer, quizNumber, date, explanation } = data.allSitePage.edges[0].node.context;
+  const [showResult, setShowResult] = useState(false);
+  const [showNoSelectionMsg, setShowNoSelectionMsg] = useState(false);
+
+  const [selectedAnswer, setSelectedAnswer] = useState();
+
+  return (
+    <>
+      <NavBar />
+      <Layout>
+        <div>
+          <h2>Daily AWS Quiz {quizNumber} ({date})</h2>
+          <p>{question}</p>
+          {Object.entries(answers).map(([key, answer]) => {
+            return (
+              <>
+                <input type="radio" name="answerSelection" htmlFor={key} value={key} onChange={(value) => { setSelectedAnswer(value) }} />
+                <label htmlFor={key}>{answer}</label>
+                <br />
+              </>
+            );
+          })}
+        </div>
+        <button id="submit" onClick={() => {
+          if (selectedAnswer) {
+            setShowResult(true)
+          } else {
+            setShowNoSelectionMsg(true)
+          }
+        }}>Show Answer</button>
+        {(showNoSelectionMsg && !showResult) &&
+          <p className={styles.errorMsg}>* Please select an answer</p>
+        }
+        {showResult &&
+          <div id="results">
+            <p>The correct answer is <strong>{answers[correctAnswer]}</strong>. <strong>{explanation}</strong>
+            </p>
+          </div>
+        }
+      </Layout>
+    </>
+  )
+};
+
+export default Quiz
+
+export const query = graphql`
+  query ($path: String!) {
+    allSitePage(filter: {path: {eq: $path } }) {
+      edges {
+        node {
+          id
+          context {
+            slug
+            question
+            correctAnswer
+            answers {
+              a
+              b
+              c
+            }
+            date
+            quizNumber
+            explanation
+          }
+          path
+        }
+      }
+    }
+  }
+`
