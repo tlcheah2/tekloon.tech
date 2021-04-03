06057557862c17db86bfd1e4010c6d8884019568
Tek Loon <tekloon.1991@gmail.com>
2021-04-03 15:16:31 +0800

    feat: add cover to Post Component

--- src/templates/Post.js
+++ src/templates/Post.js
@@ -6,37 +6,43 @@ import SEO from '../components/Seo';
 import ArticleSeo from '../components/ArticleSeo';
 import SubscribeForm from '../components/SubscribeForm';
 import styles from './Post.module.css';
-import { defineCustomElements as deckDeckGoHighlightElement } from '@deckdeckgo/highlight-code/dist/loader'; 
+import { defineCustomElements as deckDeckGoHighlightElement } from '@deckdeckgo/highlight-code/dist/loader';
 deckDeckGoHighlightElement();
 
 export default ({ data }) => {
-    console.log('styles', styles);
-    const post = data.markdownRemark;
-    const { siteMetadata } = data.site;
-    const { slug } = post.fields;
-    return (
-        <>
-            <SEO title={post.frontmatter.title} description={post.excerpt} isBlogPost={true} path={slug} />
-            <ArticleSeo 
-              title={post.frontmatter.title} 
-              description={post.excerpt}
-              url={`${siteMetadata.url}${slug}`}
-              isBlogPost={true}
-              datePublished={post.frontmatter.date}
-              author={siteMetadata.author}
-              imageUrl={siteMetadata.image}
-              organization={siteMetadata.organization}
-            />
-            <NavBar />
-            <Layout>
-                <h1 className={styles.postTitle}>{post.frontmatter.title}</h1>
-                <p className={styles.publishedDate}>Published {post.frontmatter.date}</p>
-                <hr />
-                <div dangerouslySetInnerHTML={{ __html: post.html }} />
-                <SubscribeForm />
-            </Layout>
-        </>
-    );
+  const post = data.markdownRemark;
+  const { siteMetadata } = data.site;
+  const { slug } = post.fields;
+  return (
+    <>
+      <SEO
+        title={post.frontmatter.title}
+        description={post.excerpt}
+        isBlogPost={true}
+        path={slug}
+        postCoverImage={post.frontmatter.cover}
+      />
+      <ArticleSeo
+        title={post.frontmatter.title}
+        description={post.excerpt}
+        url={`${siteMetadata.url}${slug}`}
+        isBlogPost={true}
+        datePublished={post.frontmatter.date}
+        author={siteMetadata.author}
+        imageUrl={siteMetadata.image}
+        organization={siteMetadata.organization}
+        postCoverImage={post.frontmatter.cover}
+      />
+      <NavBar />
+      <Layout>
+        <h1 className={styles.postTitle}>{post.frontmatter.title}</h1>
+        <p className={styles.publishedDate}>Published {post.frontmatter.date}</p>
+        <hr />
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <SubscribeForm />
+      </Layout>
+    </>
+  );
 };
 
 export const query = graphql`
@@ -46,6 +52,7 @@ export const query = graphql`
       frontmatter {
         title
         date
+        cover
       }
       excerpt
       fields {
